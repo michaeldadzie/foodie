207abd666dd359b0065a5b6d841045572531219b
dadzi <dadziemikke2@gmail.com>
2022-07-24 16:11:06 +0000

    Added data class

---
+++ lib/providers/data_class.dart
@@ -0,0 +1,13 @@
+import '../exports.dart';
+
+class DataClass extends ChangeNotifier {
+  DataModel? food;
+  bool loading = false;
+
+  getData() async {
+    loading = true;
+    food = (await getFoodieData());
+    loading = false;
+    notifyListeners();
+  }
+}
