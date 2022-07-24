448524b0c0fceff6d419e8f6f69219632117073a
dadzi <dadziemikke2@gmail.com>
2022-07-24 16:12:08 +0000

    Added export paths

---
+++ lib/exports.dart
@@ -0,0 +1,17 @@
+// Packages
+export 'package:flutter/material.dart';
+export 'package:provider/provider.dart';
+
+// Models
+export 'package:foodie/models/data_model.dart';
+
+// Screens
+export 'package:foodie/screens/home.dart';
+
+// Providers
+export 'package:foodie/providers/data_class.dart';
+
+// Services
+export 'package:foodie/services/service_class.dart';
+
+// Widgets
